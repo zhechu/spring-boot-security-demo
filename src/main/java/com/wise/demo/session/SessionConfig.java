0c953d21bfdee0a1a043738df2971354776d0aa9
zhechu <ling-yu-wang@qq.com>
2019-05-12 23:36:27 +0800

    自定义Cookie配置

--- src/main/java/com/wise/demo/session/SessionConfig.java
+++ src/main/java/com/wise/demo/session/SessionConfig.java
@@ -7,16 +7,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.session.InvalidSessionStrategy;
 import org.springframework.security.web.session.SessionInformationExpiredStrategy;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 import com.wise.demo.config.properties.SecurityProperties;
 
 /**
- * session bean 相关配置
+ * session 相关配置
  * @author lingyuwang
  *
  */
 @Configuration
-public class SessionBeanConfig {
+public class SessionConfig {
 
 	@Autowired
 	private SecurityProperties securityProperties;
@@ -51,5 +53,17 @@ public class SessionBeanConfig {
 	public LogoutSuccessHandler logoutSuccessHandler(){
 		return new CustomLogoutSuccessHandler(securityProperties.getBrowser().getSignOutUrl());
 	}
-	
+
+	/**
+	 * cookie 序列化配置
+	 * @return
+	 */
+    @Bean
+    @ConditionalOnMissingBean(CookieSerializer.class)
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName(securityProperties.getBrowser().getSession().getCookieName()); 
+        serializer.setCookiePath(securityProperties.getBrowser().getSession().getCookiePath()); 
+        return serializer;
+    }
 }
